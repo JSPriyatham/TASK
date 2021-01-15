6886b6181bcfcd08d10d68164db7432b54ba4323
JSPriyatham <j.priyatham@gmail.com>
2021-01-15 10:26:55 +0530

    latest change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,14 @@ export class AppComponent {
 
   public imageArray = [
     'https://i.pinimg.com/originals/cf/1f/30/cf1f30a2f69c799469d2405c78d349a5.jpg',
-    'https://lh3.googleusercontent.com/proxy/FBSord6sYWRfR7Usolj9TL60PboIWGW76RTM0TXP0zBzRhdbZg2baldak5AEVcYTHll0kHgnnwxcnbwsI9mZmtPY',
+    'https://png.pngitem.com/pimgs/s/3-32716_baby-jungle-animals-giraffe-clip-art-animal-clipart.png',
     'https://www.pngitem.com/pimgs/m/72-727500_kitten-clipart-real-animal-pencil-and-in-color.png',
     'https://www.vippng.com/png/detail/66-666165_jungle-baby-animals-clipart.png',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScLOx4UlPVf1w1QSZlH34JjFvEdmQfidXcgg&usqp=CAU'
   ]
 
   public nameArray = [
-    'Kitten', 'Zebra', 'Elephant', 'Penguin', 'Dolphin'
+    'Kitten', 'Zebra', 'Elephant', 'Penguin', 'Turtle'
   ]
 
 
